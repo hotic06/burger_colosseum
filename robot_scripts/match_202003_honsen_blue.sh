2f574625848f77fbee1ddf1eb6b0416ea8308e74
Ubuntu <hiroki@e-mail.jp>
2020-08-31 16:15:38 +0000

    fix blue

--- robot_scripts/match_202003_honsen_blue.sh
+++ robot_scripts/match_202003_honsen_blue.sh
@@ -5,7 +5,7 @@ SCRIPT_DIR=$(cd $(dirname $0); pwd)
 BURGER_DIR=~/catkin_ws
 JUDGE_IP=192.168.0.100
 
-${SCRIPT_DIR}/display_name.sh `${SCRIPT_DIR}/get_git_info.sh`
+${SCRIPT_DIR}/display_name.sh `${SCRIPT_DIR}/get_git_info.sh $BURGER_DIR/src/burger_war/`
 
 
 bash ${BURGER_DIR}/src/burger_war/judge/test_scripts/set_running.sh ${JUDGE_IP}:5000
