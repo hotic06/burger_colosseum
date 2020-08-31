45abdef17087d0791376ed336a65d314440d69b6
Ubuntu <ubuntu@RHC3rd-00.lxd>
2020-08-31 06:05:39 +0000

    add blue side

---
+++ robot_scripts/match_202003_honsen_blue.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# directory configuration
+SCRIPT_DIR=$(cd $(dirname $0); pwd)
+BURGER_DIR=~/catkin_ws
+JUDGE_IP=192.168.0.100
+
+
+bash ${BURGER_DIR}/src/burger_war/judge/test_scripts/set_running.sh ${JUDGE_IP}:5000
+
+roslaunch ${SCRIPT_DIR}/../launch/final_blue.launch
+
